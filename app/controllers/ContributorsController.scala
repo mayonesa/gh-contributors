1cca69cb83b82f34607381c783065c6795dda674
mayonesa <juanchoj1024@gmail.com>
2021-11-23 01:35:53 -0500

    ehcache for entrypoint's sole value

--- app/controllers/ContributorsController.scala
+++ app/controllers/ContributorsController.scala
@@ -4,6 +4,7 @@ import javax.inject._
 import scala.concurrent.ExecutionContext
 import play.api.mvc._
 import play.api.libs.json._
+import play.api.cache._
 import models.{ContributorInfo, GitHub}
 
 import scala.util.{Failure, Success, Try}
@@ -17,7 +18,7 @@ import scala.util.{Failure, Success, Try}
  * @param exec `ExecutionContext` to execute the asynchronous code
  */
 @Singleton
-class ContributorsController @Inject()(gh: GitHub, val controllerComponents: ControllerComponents)
+class ContributorsController @Inject()(gh: GitHub, cache: AsyncCacheApi, val controllerComponents: ControllerComponents)
                                       (implicit exec: ExecutionContext) extends BaseController {
   private implicit val contributorWrites: OWrites[ContributorInfo] = Json.writes[ContributorInfo]
 
@@ -27,7 +28,12 @@ class ContributorsController @Inject()(gh: GitHub, val controllerComponents: Con
    * Assumption: number of commits per contributor will not exceed `Int.MaxValue` (2,147,483,647)
    */
   def byNContributions(orgName: String): Action[AnyContent] = Action.async {
-    gh.contributorsByNCommits(orgName).transform {
+    // all other caching-potentials are covered by EhCache on the WS client
+    val contributorsFut = cache.getOrElseUpdate[Vector[ContributorInfo]](s"contributors.$orgName") {
+      gh.contributorsByNCommits(orgName)
+    }
+
+    contributorsFut.transform {
       case Success(contributors) => Try(Ok(Json.toJson(contributors)))
 
       // failures at this point will very likely stem from records not being found or user agent not authenticated
