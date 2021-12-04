f1554df15c0c0713990748e477a6cbe9c83bd650
mayonesa <juanchoj1024@gmail.com>
2021-12-04 16:18:43 -0500

    inserts binarily

--- app/models/package.scala
+++ app/models/package.scala
@@ -1,8 +1,27 @@
-package object models {
-  def descInsert[T](v: Vector[T], t: T)(implicit ord: Ordering[T]): Vector[T] = {
-    // slight-optimization opportunity: binary search (instead of linear). effort started in binary_search_insert branch
-    val (prefix, suffix) = v.partition(ord.gteq(_, t))
+import scala.annotation.tailrec
 
-    (prefix :+ t) ++ suffix
+package object models {
+  // time: O(log n)
+  def descInsert[T](sortedV: Vector[T], t: T)(implicit ord: Ordering[T]): Vector[T] = {
+    @tailrec
+    def loop(s: Int, e: Int): Int =
+      if (s >= e)
+        if (ord.gt(t, sortedV(s))) s
+        else s + 1
+      else {
+        val m = (s + e) / 2
+        val vm = sortedV(m)
+        if (ord.equiv(t, vm)) m
+        else if (ord.gt(t, vm)) loop(s, m - 1)
+        else loop(m + 1, e)
+      }
+
+    if (sortedV.isEmpty)
+      Vector(t)
+    else {
+      val insertIdx = loop(0, sortedV.size - 1)
+      val (prefix, suffix) = sortedV.splitAt(insertIdx)
+      (prefix :+ t) ++ suffix
+    }
   }
 }
--- test/models/packageSpec.scala
+++ test/models/packageSpec.scala
@@ -19,5 +19,45 @@ class packageSpec extends PlaySpec {
         descInsert(acc, int)
       } mustBe descBy1
     }
+    "random-spacing list" in {
+      val v = Vector(39, 23, 23, 19, 18, 1, 0)
+      val fifteen = 15
+      descInsert(v, fifteen) mustBe Vector(39, 23, 23, 19, 18, fifteen, 1, 0)
+    }
+    "insert at beginning of list" in {
+      val v = Vector(39, 23, 23, 19, 18, 1, 0)
+      val hundred = 100
+      descInsert(v, hundred) mustBe Vector(hundred, 39, 23, 23, 19, 18, 1, 0)
+    }
+    "insert right before list" in {
+      val v = Vector(39, 23, 23, 19, 18, 1, 0)
+      val forty = 40
+      descInsert(v, forty) mustBe Vector(forty, 39, 23, 23, 19, 18, 1, 0)
+    }
+    "insert right after beginning list" in {
+      val v = Vector(39, 23, 23, 19, 18, 1, 0)
+      val thirty8 = 38
+      descInsert(v, thirty8) mustBe Vector(39, thirty8, 23, 23, 19, 18, 1, 0)
+    }
+    "insert end of list" in {
+      val v = Vector(39, 23, 23, 19, 18, 1)
+      val zero = 0
+      descInsert(v, zero) mustBe Vector(39, 23, 23, 19, 18, 1, 0)
+    }
+    "insert right before end of list" in {
+      val v = Vector(39, 23, 23, 19, 18, 0)
+      val one = 1
+      descInsert(v, one) mustBe Vector(39, 23, 23, 19, 18, one, 0)
+    }
+    "insert dupe" in {
+      val v = Vector(39, 23, 23, 19, 18, 1, 0)
+      val nineteen = 19
+      descInsert(v, nineteen) mustBe Vector(39, 23, 23, nineteen, 19, 18, 1, 0)
+    }
+    "insert dupe w/ adjacents" in {
+      val v = Vector(39, 23, 23, 20, 19, 18, 1, 0)
+      val nineteen = 19
+      descInsert(v, nineteen) mustBe Vector(39, 23, 23, 20, nineteen, 19, 18, 1, 0)
+    }
   }
 }
